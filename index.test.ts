0d77d099461d36cd1cf1b11e554818149b5d631b
Markus Sandén <markus@doctrin.se>
2021-10-16 01:03:54 +0200

    1st commit

---
+++ index.test.ts
@@ -0,0 +1,129 @@
+import {
+  assertEquals,
+  assertRejects,
+  assertThrows,
+} from "https://deno.land/std@0.111.0/testing/asserts.ts";
+import { AsyncMemCache } from "./index.ts";
+import { FakeTime } from "https://deno.land/x/mock@0.10.1/mod.ts";
+
+Deno.test("returns promise", async () => {
+  const c = new AsyncMemCache<string>(1);
+
+  const f = () => Promise.resolve("ok");
+
+  const result = await c.getOrAdd("myKey", f);
+
+  assertEquals(result, "ok");
+  c.destroy();
+});
+Deno.test("returns different promises", async () => {
+  const c = new AsyncMemCache<string>(1);
+
+  const f = (value: string) => () => Promise.resolve(value);
+
+  assertEquals(await c.getOrAdd("myKey", f("ok")), "ok");
+  assertEquals(await c.getOrAdd("myKey2", f("ok2")), "ok2");
+  c.destroy();
+});
+Deno.test("will cache", async () => {
+  const c = new AsyncMemCache<string>(1000);
+
+  const f = (value: string) => () => Promise.resolve(value);
+
+  assertEquals(await c.getOrAdd("myKey", f("first")), "first");
+  assertEquals(await c.getOrAdd("myKey", f("second")), "first");
+  c.destroy();
+});
+
+Deno.test("does not return expired value", async () => {
+  const t = new FakeTime("2020-01-01");
+  const c = new AsyncMemCache<string>(5);
+
+  try {
+    const f = (value: string) => () => Promise.resolve(value);
+
+    assertEquals(await c.getOrAdd("myKey", f("first")), "first");
+    t.tick(10);
+    assertEquals(await c.getOrAdd("myKey", f("second")), "second");
+  } finally {
+    c.destroy();
+    t.restore();
+  }
+});
+
+Deno.test("clears expired value", async () => {
+  const t = new FakeTime("2020-01-01");
+  const c = new AsyncMemCache<string>(5);
+
+  try {
+    const f = (value: string) => () => Promise.resolve(value);
+
+    await c.getOrAdd("myKey", f("first"));
+
+    assertEquals(c.length, 1);
+    t.tick(5 * AsyncMemCache.INTERVAL_MULTIPLIER + 1);
+    assertEquals(c.length, 0);
+  } finally {
+    c.destroy();
+    t.restore();
+  }
+});
+
+Deno.test("calls getFromSource only once while pending", async () => {
+  const c = new AsyncMemCache<string>(5);
+
+  try {
+    let calls = 0;
+    const f = (value: string) => () =>
+      new Promise<string>((resolve, _reject) => {
+        calls++;
+        setTimeout(() => resolve(value), 1);
+      });
+
+    const p1 = c.getOrAdd("myKey", f("first"));
+    const p2 = c.getOrAdd("myKey", f("second"));
+    const val1 = await p1;
+    const val2 = await p2;
+
+    assertEquals(val1, "first");
+    assertEquals(val2, "first");
+    assertEquals(calls, 1);
+  } finally {
+    c.destroy();
+  }
+});
+
+Deno.test("handles failure", async () => {
+  const c = new AsyncMemCache<string>(5);
+
+  try {
+    const f = (value: Error) => () => Promise.reject(value);
+
+    await assertRejects(
+      () => c.getOrAdd("myKey", f(new Error("boom"))),
+      Error,
+      "boom"
+    );
+    assertEquals<number>(c.length, 0);
+  } finally {
+    c.destroy();
+  }
+});
+
+Deno.test("method shouldCache", async () => {
+  const c = new AsyncMemCache<string>(5, (value: string) => !!value);
+
+  try {
+    const f = () => Promise.resolve("");
+
+    assertEquals<string>(await c.getOrAdd("myKey", f), "");
+    assertEquals<number>(c.length, 0);
+  } finally {
+    c.destroy();
+  }
+});
+
+Deno.test("invalid ttlMs", () => {
+  assertThrows(() => new AsyncMemCache(0));
+  assertThrows(() => new AsyncMemCache(-1));
+});
---
+++ index.ts
@@ -0,0 +1,85 @@
+/**
+ * AsyncMemCache will cache a record of async calls.
+ * Old values will expire and eventually be
+ * cut off for garbage collection.
+ *
+ * Remember to destroy when discarded to avoid interval memory leak:
+ * @example
+ *    const c = AsyncMemCache(5);
+ *    // operations
+ *    c.destroy();
+ *    return
+ */
+export class AsyncMemCache<T> {
+  private storage: Record<string, { promise: Promise<T>; resolvedAt?: Date }> =
+    {};
+  private interval: number;
+
+  static INTERVAL_MULTIPLIER = 5;
+
+  constructor(
+    public ttlMs: number,
+    public shouldCache: (value: T) => boolean = () => true
+  ) {
+    if (this.ttlMs <= 0) {
+      throw new RangeError("ttlMs not a positive number");
+    }
+    this.interval = setInterval(() => {
+      Object.keys(this.storage).forEach((key) => {
+        if (this.expired(this.storage[key])) {
+          delete this.storage[key];
+        }
+      });
+    }, ttlMs * AsyncMemCache.INTERVAL_MULTIPLIER);
+  }
+
+  private expired({ resolvedAt }: { resolvedAt?: Date }): boolean {
+    if (!resolvedAt) {
+      return false;
+    }
+    return resolvedAt.valueOf() < new Date().valueOf() - this.ttlMs;
+  }
+
+  destroy() {
+    clearInterval(this.interval);
+  }
+
+  get length() {
+    return Object.keys(this.storage).length;
+  }
+
+  getOrAdd(key: string, getFromSource: () => Promise<T>): Promise<T> {
+    if (!(key in this.storage) || this.expired(this.storage[key])) {
+      const innerPromise = getFromSource();
+
+      const outerPromise = new Promise<T>((resolve, reject) => {
+        const executor = async () => {
+          try {
+            const result = await innerPromise;
+
+            if (!this.shouldCache(result)) {
+              delete this.storage[key];
+
+              resolve(result);
+              return;
+            }
+
+            this.storage[key].resolvedAt = new Date();
+
+            resolve(result);
+          } catch (error) {
+            delete this.storage[key];
+
+            reject(error);
+          }
+        };
+        executor();
+      });
+
+      this.storage[key] = { promise: outerPromise };
+    }
+
+    // this will execute _before_ the executor has run to completion
+    return this.storage[key].promise;
+  }
+}
